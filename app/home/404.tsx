67abfa023edb8e36bb028dbab1533f573fa764f6
William McGonagle <wpmcgonagle@gmail.com>
2023-12-04 16:55:40 -0500

    Removed About

--- app/home/404.tsx
+++ app/home/404.tsx
@@ -10,7 +10,7 @@ const NotFoundPage = () => (
         <div className="flex justify-center font-light text-6xl text-gray-500 text-opacity-75">
           404
         </div>
-        <div className="flex justify-center font-medium text-lg my-2 text-gray-800">The page you're looking for is not found.</div>
+        <div className="flex justify-center font-medium text-lg my-2 text-gray-800">The page you&apos;re looking for is not found.</div>
         <hr />
         <div className="px-6 py-4">
           Make sure the address is correct. If you think this is a mistake,
