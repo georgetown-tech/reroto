76b6968526e3a107be563264dd36848b12721cf9
William McGonagle <wpmcgonagle@gmail.com>
2023-12-04 22:50:31 -0500

    Fixing TS

--- components/add-transcription-button.tsx
+++ components/add-transcription-button.tsx
@@ -13,7 +13,7 @@ export default function AddTranscriptionButton({
   return (
     <button
       onClick={() => modal?.show(children)}
-      class="flex items-center justify-center gap-1 rounded-lg bg-primary-700 px-4 py-2 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+      className="flex items-center justify-center gap-1 rounded-lg bg-primary-700 px-4 py-2 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
     >
       <Plus size={18} />
       Add Transcription
