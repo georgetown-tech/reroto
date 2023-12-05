4f7365f7ef213e89cb866a6f7493f05ecd4c358d
William McGonagle <wpmcgonagle@gmail.com>
2023-12-04 23:43:20 -0500

    Fixing TS

--- components/nav.tsx
+++ components/nav.tsx
@@ -33,24 +33,6 @@ import { getSiteFromPostId } from "@/lib/actions";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 
-const externalLinks = [
-  // {
-  //   name: "Deploy your own",
-  //   href: "https://vercel.com/templates/next.js/platforms-starter-kit",
-  //   icon: (
-  //     <svg
-  //       width={18}
-  //       viewBox="0 0 76 76"
-  //       fill="none"
-  //       xmlns="http://www.w3.org/2000/svg"
-  //       className="py-1 text-black dark:text-white"
-  //     >
-  //       <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="currentColor" />
-  //     </svg>
-  //   ),
-  // },
-];
-
 export default function Nav({ children }: { children: ReactNode }) {
   const segments = useSelectedLayoutSegments();
   const { id } = useParams() as { id?: string };
@@ -212,7 +194,7 @@ export default function Nav({ children }: { children: ReactNode }) {
               ) : (
                 <details
                   key={name}
-                  class="group [&_summary::-webkit-details-marker]:hidden"
+                  className="group [&_summary::-webkit-details-marker]:hidden"
                   open
                 >
                   <summary
@@ -246,27 +228,6 @@ export default function Nav({ children }: { children: ReactNode }) {
               ),
             )}
           </div>
-        </div>
-        <div>
-          <div className="grid gap-1">
-            {externalLinks.map(({ name, href, icon }) => (
-              <a
-                key={name}
-                href={href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center justify-between rounded-lg px-2 py-1.5 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800"
-              >
-                <div className="flex items-center space-x-3">
-                  {icon}
-                  <span className="text-sm font-medium">{name}</span>
-                </div>
-                <p>↗</p>
-              </a>
-            ))}
-          </div>
-          <div className="my-2 border-t border-stone-200 dark:border-stone-700" />
-          {children}
         </div>
       </div>
     </>
