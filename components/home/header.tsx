cba712727ee779e5b3fbc80bf77d1af303254c3d
William McGonagle <wpmcgonagle@gmail.com>
2023-12-05 20:31:30 -0500

    Changed Sign-Up

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -47,7 +47,7 @@ export default function Header({}) {
             <Link
               className="whitespace-nowrap rounded bg-primary p-3 px-8 font-bold text-white no-underline"
               // activeClassName="whitespace-nowrap"
-              href="http://app.localhost:3000/login"
+              href="http://app.reroto.com/login"
             >
               Sign Up
             </Link>
