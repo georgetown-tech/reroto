1bd24bd8cba0173e2fd935fa6a63be8b1cd6c76c
William McGonagle <wpmcgonagle@gmail.com>
2024-02-28 21:35:48 -0500

    Removed Static Generation for Endpoints

--- app/[domain]/[slug]/page.tsx
+++ app/[domain]/[slug]/page.tsx
@@ -47,38 +47,38 @@ export async function generateMetadata({
 }
 
 export async function generateStaticParams() {
-  const allPosts = await prisma.post.findMany({
-    select: {
-      slug: true,
-      site: {
-        select: {
-          subdomain: true,
-          customDomain: true,
-        },
-      },
-    },
-    // feel free to remove this filter if you want to generate paths for all posts
-    // where: {
-    //   site: {
-    //     subdomain: "demo",
-    //   },
-    // },
-  });
+  // const allPosts = await prisma.post.findMany({
+  //   select: {
+  //     slug: true,
+  //     site: {
+  //       select: {
+  //         subdomain: true,
+  //         customDomain: true,
+  //       },
+  //     },
+  //   },
+  //   // feel free to remove this filter if you want to generate paths for all posts
+  //   // where: {
+  //   //   site: {
+  //   //     subdomain: "demo",
+  //   //   },
+  //   // },
+  // });
 
-  const allPaths = allPosts
-    .flatMap(({ site, slug }) => [
-      site?.subdomain && {
-        domain: `${site.subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`,
-        slug,
-      },
-      site?.customDomain && {
-        domain: site.customDomain,
-        slug,
-      },
-    ])
-    .filter(Boolean);
+  // const allPaths = allPosts
+  //   .flatMap(({ site, slug }) => [
+  //     site?.subdomain && {
+  //       domain: `${site.subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`,
+  //       slug,
+  //     },
+  //     site?.customDomain && {
+  //       domain: site.customDomain,
+  //       slug,
+  //     },
+  //   ])
+  //   .filter(Boolean);
 
-  return allPaths;
+  return [];
 }
 
 export default async function SitePostPage({
