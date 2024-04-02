78f0885a814bb4c684415278c609499b3c29ea78
William McGonagle <wpmcgonagle@gmail.com>
2024-04-02 15:26:48 -0400

    Removing Dead Code

--- components/overview-sites-cta.tsx
+++
@@ -1,9 +0,0 @@
-// import { getSession } from "@/lib/auth";
-import prisma from "@/lib/prisma";
-import CreateSiteButton from "./create-site-button";
-import CreateSiteModal from "./modal/create-site";
-import Link from "next/link";
-
-export default async function OverviewSitesCTA() {
-  return <></>;
-}
