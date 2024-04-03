c16df248bbc4e55456fc63573154ac1c23520649
William McGonagle <wpmcgonagle@gmail.com>
2024-04-02 20:09:15 -0400

    Switched from `value` to `defaultValue`

--- app/app/(dashboard)/team/page.tsx
+++ app/app/(dashboard)/team/page.tsx
@@ -89,7 +89,7 @@ export default async function TeamOverview({
                 <input
                   type="text"
                   name="q"
-                  value={searchParams.q}
+                  defaultValue={searchParams.q}
                   id="simple-search"
                   className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 pl-10 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
                   placeholder="Search"
